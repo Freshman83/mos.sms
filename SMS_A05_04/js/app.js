5d198795f55def50984bc9df6110b08a115f70a5
Sascha Niklas <sascha.niklas@karlstorz.com>
2018-05-04 22:11:22 +0200

    Update

--- SMS_A05_04/js/app.js
+++ SMS_A05_04/js/app.js
@@ -3,5 +3,5 @@ var controller = new Controller(model);
 $(controller.onLoaded);
 
 function changeTheme(theme){   
-    $("#pageDiv").removeClass('ui-page-theme-a ui-page-theme-b').addClass('ui-page-theme-' + theme);
+    $("#converter").removeClass('ui-page-theme-a ui-page-theme-b').addClass('ui-page-theme-' + theme);
 }
